b7e577b749aa2b75a58d90f7fcdf18fce6acf902
BrunoLeon007 <brunob734@gmail.com>
2025-05-28 19:41:34 -0300

    Add files via upload

---
+++ Mesestacao.java
@@ -0,0 +1,79 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Mesestacao {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+
+		    System.out.print("Digite um número de 1 a 12: ");
+		    int mes = scanner.nextInt();
+
+		    String nomeMes = "";
+		    String estacao = "";
+
+		    switch (mes) {
+		      case 1:
+		        nomeMes = "Janeiro";
+		        estacao = "Verão";
+		        break;
+		      case 2:
+		        nomeMes = "Fevereiro";
+		        estacao = "Verão";
+		        break;
+		      case 3:
+		        nomeMes = "Março";
+		        estacao = "Outono";
+		        break;
+		      case 4:
+		        nomeMes = "Abril";
+		        estacao = "Outono";
+		        break;
+		      case 5:
+		        nomeMes = "Maio";
+		        estacao = "Outono";
+		        break;
+		      case 6:
+		        nomeMes = "Junho";
+		        estacao = "Inverno";
+		        break;
+		      case 7:
+		        nomeMes = "Julho";
+		        estacao = "Inverno";
+		        break;
+		      case 8:
+		        nomeMes = "Agosto";
+		        estacao = "Inverno";
+		        break;
+		      case 9:
+		        nomeMes = "Setembro";
+		        estacao = "Primavera";
+		        break;
+		      case 10:
+		        nomeMes = "Outubro";
+		        estacao = "Primavera";
+		        break;
+		      case 11:
+		        nomeMes = "Novembro";
+		        estacao = "Primavera";
+		        break;
+		      case 12:
+		        nomeMes = "Dezembro";
+		        estacao = "Verão";
+		        break;
+		      default:
+		        System.out.println("Mês inválido!");
+		        return;
+		    }
+
+		    System.out.println("Mês: " + nomeMes);
+		    System.out.println("Estação: " + estacao);
+
+		    scanner.close();
+		  }
+		}
+
+	
+
+
